79ef579a4d89fc8b37aa813e31c2005b8092d42d
Nnamdi-Echegz <121641724+Nnamdi-Echegz@users.noreply.github.com>
2023-02-08 18:37:40 -0500

    Adding compound buttons

--- app/src/main/java/algonquin/cst2335/eche0011/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/eche0011/MainActivity.java
@@ -3,12 +3,17 @@ package algonquin.cst2335.eche0011;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imgView;
+    Switch sw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
